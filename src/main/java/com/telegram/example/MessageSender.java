bc55f37af76aedcda1dbc5bc309739030758525e
JAPL <andrespuerto1993@hotmail.com>
2023-05-15 14:53:43 -0600

    Erease commented lines

--- src/main/java/com/telegram/example/MessageSender.java
+++ src/main/java/com/telegram/example/MessageSender.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.request.ForceReply;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
